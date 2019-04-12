77ba34682f8e8ef7b2857f9af66c9ee17ee11d0f
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-12 19:10:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+
+
+def prime?(int)
+ 
+ if int < 0
+   false
+     
+  elsif int == [2, 3, 7, 9, 11, 13, 17, 19]
+    true
+    
+  else  
+    
+ end
+  
+  x = 2
+  until x == 50
+    (int % x == 0)
+      x += 1
+    break if true
+        false
+  
+  end 
+end
