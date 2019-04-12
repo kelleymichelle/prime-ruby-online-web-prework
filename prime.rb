4657f13d2add4d7929e25c2f88109560c6a2b2b9
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-12 19:44:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,23 +1,13 @@
-
+require 'pry'
 
 def prime?(int)
  
  if int < 2
   return false
      
-  elsif int == [2, 3, 7, 9, 11, 13, 17, 19]
-    return true
-    
-  else  
-    
- end
-  
-  x = 2
-  until x == 50
-    (int % x == 0)
-      x += 1
-    break if true
-        false
-  
+ else 
+   
+  (2..int-1).to_a
+  binding.pry
   end 
 end
